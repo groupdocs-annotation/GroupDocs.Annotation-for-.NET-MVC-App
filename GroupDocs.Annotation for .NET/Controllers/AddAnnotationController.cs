06b0664200c1b394f6a89ff9e0ee86b86caaa277
Saqib Masood <saqib@masood.pk>
2018-04-11 14:59:00 +0500

    Fix error when annotation is added

--- GroupDocs.Annotation for .NET/Controllers/AddAnnotationController.cs
+++ GroupDocs.Annotation for .NET/Controllers/AddAnnotationController.cs
@@ -8,6 +8,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -29,10 +30,8 @@ namespace GroupDocs.Annotation_for.NET.Controllers
             Document doc = documentDataHandler.GetDocument(filename);
             long documentId = doc != null ? doc.Id : imageHandler.CreateDocument(filename);
 
-            //StreamReader stream = new StreamReader(Request.InputStream);
-            //string x = stream.ReadToEnd();  // added to view content of input stream
-
-            AnnotationInfo annotation = new AnnotationInfo(); //Request.InputStream as AnnotationInfo;
+            Request.InputStream.Seek(0, SeekOrigin.Begin);
+            AnnotationInfo annotation = new JsonSerializer().Deserialize<AnnotationInfo>(new JsonTextReader(new StreamReader(Request.InputStream)));
             annotation.DocumentGuid = documentId;
             CreateAnnotationResult result = imageHandler.CreateAnnotation(annotation);
             return Content(JsonConvert.SerializeObject(
@@ -44,3 +43,4 @@ namespace GroupDocs.Annotation_for.NET.Controllers
         }
     }
 }
+
